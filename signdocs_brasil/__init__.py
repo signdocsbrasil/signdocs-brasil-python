f5ab23c329eed9493a0ccbfc32105af396261506
SignDocs Brasil <administrativo@signdocs.com.br>
2026-07-29 23:06:59 -0300

    Release v1.7.0
    Bump version 1.6.1 -> 1.7.0 for the download-response
    signatureUrl/documentFormat addition.

--- signdocs_brasil/__init__.py
+++ signdocs_brasil/__init__.py
@@ -60,7 +60,7 @@ from .response_metadata import ResponseMetadata
 from .token_cache import CachedToken, InMemoryTokenCache, TokenCache
 from .webhook_verifier import verify_webhook_signature
 
-__version__ = "1.6.1"
+__version__ = "1.7.0"
 
 __all__ = [
     # Client
